f486fc3aa962ac32204e127785b0f4d30635c421
WindTraveler <13301156@bjtu.edu.cn>
2017-12-18 10:12:33 +0800

    前端final初始化

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,16 @@ import { TodosComponent } from './todos/todos.component';
 
 //服务
 import {TodoService} from "./todo.service";
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoItemComponent,
-    TodosComponent
+    TodosComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
